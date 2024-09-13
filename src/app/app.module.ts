8d832f0eee66892de5ec9edd6bbc28e9c4573233
Thamara JZ <thamisjz@gmail.com>
2024-09-12 17:55:32 -0700

    changed into no stand alone

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
+
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppModule { }
